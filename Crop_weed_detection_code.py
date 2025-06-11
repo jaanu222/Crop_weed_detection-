0e166b670ac82b8cd016a395448782efe1351b8d
jaanu222 <janavika72@gmail.com>
2025-06-11 22:31:13 +0530

    Create Crop_weed_detection_code.py

---
+++ Crop_weed_detection_code.py
@@ -0,0 +1,6 @@
+# Crop and Weed Detection - Internship Project
+
+# This is a sample placeholder Python script for the UCT Internship Project
+# In real implementation, we will use YOLOv5 and image processing
+
+print("Welcome to Crop and Weed Detection using YOLO!")
